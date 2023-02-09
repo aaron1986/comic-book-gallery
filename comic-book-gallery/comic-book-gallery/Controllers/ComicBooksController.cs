e5ffe4fe0843d040bfce3c45403776fc09ba2fb8
aaron <aaron@AARON-PC>
2023-02-09 12:41:21 +0000

    @viewbag.variables

--- comic-book-gallery/comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing SpiderMan";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final Issue</p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazba",
+                "Colours: Edger dalgado",
+                "Letters: Chris eliopulars"
+
+            };
             return View();
         }
     }
